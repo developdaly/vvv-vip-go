263e7c56ab4485324ce58dafb972e4800697fb62
Patrick Daly <patrick@developdaly.com>
2017-09-29 13:23:41 -0500

    Follow standard vvv patterns

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -1,32 +1,13 @@
-#!/bin/bash
-# Init script for a development site with a monolithic Git repo
-# v1.0
+#!/usr/bin/env bash
+# Provision WordPress Stable
 
-# Edit these variables to suit your purposes
-# ------------------------------------------
-
-# Just a human readable description of this site
-SITE_NAME="${VVV_SITE_NAME}"
-# The name (to be) used by MySQL for the DB
-DB_NAME="${VVV_SITE_NAME}"
-# The URL/domain for the site
-SITE_URL="${DOMAIN}"
-# The multisite stuff for wp-config.php
-EXTRA_CONFIG="
-
-// With Development Mode, features that do not require a
-// connection to WordPress.com servers can be activated
-// on a localhost WordPress installation for testing:
-// https://jetpack.com/support/development-mode/
-define( 'JETPACK_DEV_DEBUG', true);
-
-// Any additional site specific config should be
-// placed in vip-config/vip-config.php
-
-"
-
-# ----------------------------------------------------------------
-# You should not need to edit below this point. Famous last words.
+DOMAIN=`get_primary_host "${VVV_SITE_NAME}".test`
+DOMAINS=`get_hosts "${DOMAIN}"`
+SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
+WP_VERSION=`get_config_value 'wp_version' 'latest'`
+WP_TYPE=`get_config_value 'wp_type' "single"`
+DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
+DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
 
 RED='\e[0;31m'
 GREEN='\e[0;32m'
@@ -35,32 +16,33 @@ NC='\e[0m' # No Color
 
 echo -e "${GREEN}Commencing $SITE_NAME setup${NC}"
 
+
 # Add GitHub and GitLab to known_hosts, so we don't get prompted
 # to verify the server fingerprint.
 # The fingerprints in [this repo]/ssh/known_hosts are generated as follows:
 #
-# As the starting point for the ssh-keyscan tool, create an ASCII file 
-# containing all the hosts from which you will create the known hosts 
+# As the starting point for the ssh-keyscan tool, create an ASCII file
+# containing all the hosts from which you will create the known hosts
 # file, e.g. sshhosts.
-# Each line of this file states the name of a host (alias name or TCP/IP 
-# address) and must be terminated with a carriage return line feed 
+# Each line of this file states the name of a host (alias name or TCP/IP
+# address) and must be terminated with a carriage return line feed
 # (Shift + Enter), e.g.
-# 
+#
 # bitbucket.org
 # github.com
 # gitlab.com
-# 
+#
 # Execute ssh-keyscan with the following parameters to generate the file:
-# 
+#
 # ssh-keyscan -t rsa,dsa -f ssh_hosts >ssh/known_hosts
-# The parameter -t rsa,dsa defines the host’s key type as either rsa 
+# The parameter -t rsa,dsa defines the host’s key type as either rsa
 # or dsa.
-# The parameter -f /home/user/ssh_hosts states the path of the source 
+# The parameter -f /home/user/ssh_hosts states the path of the source
 # file ssh_hosts, from which the host names are read.
-# The parameter >ssh/known_hosts states the output path of the 
+# The parameter >ssh/known_hosts states the output path of the
 # known_host file to be created.
-# 
-# From "Create Known Hosts Files" at: 
+#
+# From "Create Known Hosts Files" at:
 # http://tmx0009603586.com/help/en/entpradmin/Howto_KHCreate.html
 mkdir -p ~/.ssh
 touch ~/.ssh/known_hosts
@@ -72,59 +54,71 @@ for KNOWN_HOST in $(cat "ssh/known_hosts"); do
 	fi
 done
 
+
+
 # Make a database, if we don't already have one
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME; GRANT ALL PRIVILEGES ON $DB_NAME.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e "\n DB operations done.\n\n"
 
-# Let's get some config in the house
-if [ ! -f "htdocs/wp-config.php" ]; then
-	wp --allow-root core download --path=htdocs
-    # Clone the repo, if it's not there already
-    echo -e "Cloning the client code repository"
-    if ! git clone https://github.com/wpcomvip/${VVV_SITE_NAME}/ wp-content
-    then
-        echo -e "${RED}Fallback:${NC} The sites name in vvv-custom.yml doesn't match a VIP Go client slug, cloning the VIP Go Skeleton instead"
-        git clone https://github.com/Automattic/vip-skeleton.git wp-content
-    else
-        echo -e "${GREEN}Success:${NC} Client code checked out succesfully"
-    fi
-    rm -rf htdocs/wp-content
-    mv wp-content htdocs/wp-content
-    echo -e "${GREEN}Success:${NC} Moved the client code repository into position"
+# Nginx Logs
+mkdir -p ${VVV_PATH_TO_SITE}/log
+touch ${VVV_PATH_TO_SITE}/log/error.log
+touch ${VVV_PATH_TO_SITE}/log/access.log
 
-    # Object cache drop-in
-    curl -s https://raw.githubusercontent.com/Automattic/wp-memcached/master/object-cache.php > htdocs/wp-content/object-cache.php
+# Install and configure the latest stable version of WordPress
+if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-load.php" ]]; then
+    echo "Downloading WordPress..."
+	noroot wp core download --version="${WP_VERSION}"
+fi
 
-	wp --allow-root core config --dbname="$DB_NAME" --dbuser=wp --dbpass=wp --dbhost="localhost" --extra-php <<PHP
-// Additional VIP Go config via vip-config.php in the
-// client site repo
-require_once( ABSPATH . '/wp-content/vip-config/vip-config.php' );
-define( 'VIP_GO_ENV', 'vvv-local-dev' );
-$EXTRA_CONFIG
+if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-config.php" ]]; then
+  echo "Configuring WordPress Stable..."
+  noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+define( 'WP_DEBUG', true );
 PHP
-    wp --allow-root core install --url="$SITE_URL" --title="$SITE_NAME" --admin_user=wordpress --admin_password=wordpress --admin_email=info@example.invalid
-    echo -e "${GREEN}Success:${NC} Installed WordPress"
+fi
 
-    # Add MU plugins in place
-    if [ ! -d "htdocs/wp-content/mu-plugins" ]; then
-        git clone --recursive --quiet https://github.com/Automattic/vip-go-mu-plugins.git htdocs/wp-content/mu-plugins
-        echo -e "${GREEN}Success:${NC} Cloned the VIP Go MU plugins repository"
-    fi
+if ! $(noroot wp core is-installed); then
+  echo "Installing WordPress Stable..."
 
-    # Everyone gets VIP Scanner
-    wp --allow-root plugin install vip-scanner
+  if [ "${WP_TYPE}" = "subdomain" ]; then
+    INSTALL_COMMAND="multisite-install --subdomains"
+  elif [ "${WP_TYPE}" = "subdirectory" ]; then
+    INSTALL_COMMAND="multisite-install"
+  else
+    INSTALL_COMMAND="install"
+  fi
+
+  noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
+
+  # Add MU plugins in place
+  if [ ! -d "${VVV_PATH_TO_SITE}/public_html/wp-content/mu-plugins" ]; then
+    git clone --recursive --quiet https://github.com/Automattic/vip-go-mu-plugins.git htdocs/wp-content/mu-plugins
+    echo -e "${GREEN}Success:${NC} Cloned the VIP Go MU plugins repository"
+  fi
+
+  # Everyone gets VIP Scanner
+  wp --allow-root plugin install vip-scanner
+
 else
-	echo "wp-config.php already exists for ${SITE_NAME}"
-	# Make sure core and VIP Scanner are up to date
-	wp --allow-root core update
-    wp --allow-root plugin update vip-scanner
-    (
-        cd htdocs/wp-content/mu-plugins
-        git pull --recurse-submodules
-    )
-    echo -e "${GREEN}Success:${NC} Updated WordPress, VIP Go MU plugins, and VIP Scanner"
+  echo "Updating WordPress Stable..."
+  cd ${VVV_PATH_TO_SITE}/public_html
+  noroot wp core update --version="${WP_VERSION}"
+
+  echo "wp-config.php already exists for ${SITE_NAME}"
+  # Make sure core and VIP Scanner are up to date
+  wp --allow-root core update
+  wp --allow-root plugin update vip-scanner
+  (
+    cd htdocs/wp-content/mu-plugins
+    git pull --recurse-submodules
+  )
+  echo -e "${GREEN}Success:${NC} Updated WordPress, VIP Go MU plugins, and VIP Scanner"
+
 fi
 
 
-# The Vagrant site setup script will restart Nginx for us
-
-echo -e "${GREEN}${SITE_NAME} init is complete${NC}"
+cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
